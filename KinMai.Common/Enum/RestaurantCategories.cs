8617b085546cc2b3c3feab587320263d4a8be1a8
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-10-31 19:30:53 +0700

    add project logic and common

---
+++ KinMai.Common/Enum/RestaurantCategories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinMai.Common.Enum
+{
+    public enum RestaurantCategories
+    {
+        ThaiFood,
+        InternationalFood,
+        StreetFood,
+        CookedToOrder,
+        HalalFood,
+        Shabu,
+        IsaanFood,
+        Noodles,
+        GrilledPork,
+        CoffeeAndTea,
+        Dessert,
+        IceCream,
+        Caffe,
+        Bakery,
+        Juice
+    }
+}
