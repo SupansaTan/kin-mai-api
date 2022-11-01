31ef5e4bc5119743d6c45d587f5581d2d04d8665
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-11-02 02:33:34 +0700

    add project dynamo db

---
+++ KinMai.AWS.DynamoDB/Interfaces/IOfficeHoursService.cs
@@ -0,0 +1,18 @@
+﻿using KinMai.AWS.DynamoDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinMai.AWS.DynamoDB.Interfaces
+{
+    public interface IOfficeHoursService
+    {
+        Task<List<OfficeHours>> GetAllAsync();
+        Task<OfficeHours> GetSingleAsync(string officeHoursId);
+        Task AddAsync(OfficeHours officeHours);
+        Task RemoveAsync(string officeHoursId);
+        Task UpdateAsync(OfficeHours officeHours);
+    }
+}
---
+++ KinMai.AWS.DynamoDB/Models/OfficeHours.cs
@@ -0,0 +1,16 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinMai.AWS.DynamoDB.Models
+{
+    [DynamoDBTable("OfficeHours")]
+    public class OfficeHours
+    {
+        [DynamoDBHashKey("OfficeHoursId")]
+        public string OfficeHoursId { get; set; }
+    }
+}
