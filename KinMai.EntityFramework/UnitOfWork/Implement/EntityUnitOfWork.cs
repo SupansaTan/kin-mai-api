b2d7afdca0d70c0371852d368c46c155819751c0
Supansa Tantulset <supansa.tantulset@gmail.com>
2022-11-30 00:44:30 +0700

    add entity framework project

---
+++ KinMai.EntityFramework/UnitOfWork/Implement/EntityUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using KinMai.EntityFramework.Models;
+using KinMai.EntityFramework.UnitOfWork.Interface;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KinMai.EntityFramework.UnitOfWork.Implement
+{
+    public class EntityUnitOfWork : IEntityUnitOfWork
+    {
+        private KinMaiContext _dbContext;
+
+        public EntityUnitOfWork(KinMaiContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<int> SaveAsync()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
