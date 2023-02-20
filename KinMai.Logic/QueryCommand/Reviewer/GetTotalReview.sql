34b457aa4450767d62cc1cabb1dfe2cc21ca4e9f
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-20 18:15:42 +0700

    add query get total review

---
+++ KinMai.Logic/QueryCommand/Reviewer/GetTotalReview.sql
@@ -0,0 +1,25 @@
+﻿select
+	(select
+		COUNT(*)
+	from "Review"
+	where "Review"."RestaurantId" = '_restaurantId') as "TotalReview",
+
+	(select
+		COUNT(*)
+	from "Review"
+	where "Review"."RestaurantId" = '_restaurantId'
+	and "Review"."ImageLink" is not null) as "TotalReviewHaveImage",
+
+	(select
+		COUNT(*)
+	from "Review"
+	where "Review"."RestaurantId" = '_restaurantId'
+	and "Review"."Comment" is not null) as "TotalReviewHaveComment",
+
+	(select
+		COUNT(*)
+	from "Review"
+	where "Review"."RestaurantId" = '_restaurantId'
+	and "Review"."FoodRecommendList" is not null) as "TotalReviewHaveFoodRecommend"
+from "Review"
+where "Review"."RestaurantId" = '_restaurantId';
