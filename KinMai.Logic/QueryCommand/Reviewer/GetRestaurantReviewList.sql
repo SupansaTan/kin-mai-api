0bb269c09e5cefaf822f46a0cbfaca7e665f866f
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-02-20 18:03:34 +0700

    add query get restaurant review list

---
+++ KinMai.Logic/QueryCommand/Reviewer/GetRestaurantReviewList.sql
@@ -0,0 +1,36 @@
+﻿select
+	u."Username" as "Username",
+	"Review"."Rating" as "Rating",
+	"Review"."Comment" as "Comment",
+	now() - "Review"."CreateAt" as "CreatedDateDiff",
+	"Review"."ImageLink" as "ImageReviewList",
+	"Review"."FoodRecommendList" as "FoodRecommendList",
+	"Review"."ReviewLabelRecommend" as "ReviewLabelList"
+from "Review"
+left join "User" u ON u."Id" = "Review"."UserId" 
+where "Review"."RestaurantId" = '_restaurantId'
+	and
+	
+	-- filter comment, food recommend
+	LOWER("Review"."Comment") LIKE ALL(string_to_array('_keyword', ' '))
+	and
+	
+	-- filter by get only review that have image
+    ('_isOnlyReviewHaveImage' = 0 OR 
+    ("Review"."ImageLink" is not null))	
+	and
+	
+	-- filter by get only review that have comment
+    ('_isOnlyReviewHaveComment' = 0 OR 
+    ("Review"."Comment" is not null))	
+	and
+	
+	-- filter by get only review that have food recommend
+    ('_IsOnlyReviewHaveFoodRecommend' = 0 OR 
+    ("Review"."FoodRecommendList" is not null))
+    and 
+    
+    -- filter by get only review that have food recommend
+    ('_rating' = 6 OR 
+    ("Review"."Rating" = 5))
+order by "CreatedDateDiff", "Rating" DESC;
