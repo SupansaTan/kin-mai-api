c88dea4906e2f61f5a4c05711ed2df639049b50c
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-03-21 16:44:44 +0700

    add test register with google acc as reviewer

--- KinMai.UnitTests/Controllers/AuthenticationControllerTest/ReviewerRegister.cs
+++ KinMai.UnitTests/Controllers/AuthenticationControllerTest/ReviewerRegister.cs
@@ -81,6 +81,53 @@ namespace KinMai.UnitTests.Controllers.AuthenticationControllerTest
         }
 
         [Fact]
+        public async Task ReviewerRegister_ReturnStatus200_WhenRegisterWithGoogleAccount()
+        {
+            // mock unit of work
+            var mockDapperUnitOfWork = new Mock<IDapperUnitOfWork>();
+            var mockS3UnitOfWork = new Mock<IS3UnitOfWork>();
+            var mockMailUnitOfWork = new Mock<IMailUnitOfWork>();
+            IAuthenticationUnitOfWork mockAuthenticationUnitOfWork = new AuthenticationUnitOfWork();
+
+            // arrange
+            var mockRequest = new ReviewerRegisterModel()
+            {
+                Email = "nampunch1@gmail.com",
+                FirstName = "Supansa",
+                LastName = "Tantulset",
+                Username = "littlepunchhz"
+            };
+
+            using (var context = new KinMaiContext(NewDbContextService.CreateNewContextOptions()))
+            {
+                // init controller
+                IEntityUnitOfWork mockEntityUnitOfWork = new EntityUnitOfWork(context);
+                ILogicUnitOfWork logicUnitOfWork = new LogicUnitOfWork(
+                    mockEntityUnitOfWork,
+                    mockDapperUnitOfWork.Object,
+                    mockAuthenticationUnitOfWork,
+                    mockS3UnitOfWork.Object,
+                    mockMailUnitOfWork.Object
+                );
+                var authenticationController = new AuthenticationController(logicUnitOfWork);
+
+                // act
+                var actualOutput = await authenticationController.ReviewerRegister(mockRequest);
+                var expectOutput = new ResponseModel<bool>()
+                {
+                    Data = true,
+                    Message = "success",
+                    Status = 200
+                };
+
+                // assert
+                Assert.Equal(actualOutput.Data, expectOutput.Data);
+                Assert.Equal(actualOutput.Message, expectOutput.Message);
+                Assert.Equal(actualOutput.Status, expectOutput.Status);
+            }
+        }
+
+        [Fact]
         public async Task ReviewerRegister_ReturnStatus400_WhenPasswordDoNotMatch()
         {
             // mock unit of work
