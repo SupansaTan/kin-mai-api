31c8cd27189571bb9d83312a894d3ac62077e765
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-03-23 01:59:54 +0700

    add test service in case update user profile

---
+++ KinMai.UnitTests/Services/AuthenticationServiceTest/UpdateUserProfile.cs
@@ -0,0 +1,106 @@
+﻿using KinMai.Authentication.UnitOfWork;
+using KinMai.Dapper.Interface;
+using KinMai.EntityFramework.Models;
+using KinMai.EntityFramework.UnitOfWork.Interface;
+using KinMai.Logic.Interface;
+using KinMai.Logic.Models;
+using KinMai.Logic.Services;
+using KinMai.Mail.UnitOfWork;
+using KinMai.S3.UnitOfWork.Interface;
+using KinMai.UnitTests.Shared;
+using Moq;
+using System.Linq.Expressions;
+
+namespace KinMai.UnitTests.Services.AuthenticationServiceTest
+{
+    public class UpdateUserProfile
+    {
+        private readonly InitConfiguration initConfiguration;
+        private Mock<IDapperUnitOfWork> mockDapperUnitOfWork;
+        private Mock<IS3UnitOfWork> mockS3UnitOfWork;
+        private Mock<IMailUnitOfWork> mockMailUnitOfWork;
+        private Mock<IAuthenticationUnitOfWork> mockAuthenticationUnitOfWork;
+        private Mock<IEntityUnitOfWork> mockEntityUnitOfWork;
+        private IAuthenticationService authenticationService;
+
+        public UpdateUserProfile()
+        {
+            initConfiguration = new InitConfiguration();
+            mockDapperUnitOfWork = new Mock<IDapperUnitOfWork>();
+            mockS3UnitOfWork = new Mock<IS3UnitOfWork>();
+            mockMailUnitOfWork = new Mock<IMailUnitOfWork>();
+            mockAuthenticationUnitOfWork = new Mock<IAuthenticationUnitOfWork>();
+            mockEntityUnitOfWork = new Mock<IEntityUnitOfWork>();
+            authenticationService = new AuthenticationService(
+                mockEntityUnitOfWork.Object,
+                mockAuthenticationUnitOfWork.Object,
+                mockDapperUnitOfWork.Object,
+                mockS3UnitOfWork.Object,
+                mockMailUnitOfWork.Object
+            );
+        }
+
+        [Fact]
+        public async Task UpdateUserProfile_ReturnTrue_WhenUserIsExist()
+        {
+            // arrange
+            var mockUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = "nampunch1@gmail.com",
+                FirstName = "Supansa",
+                LastName = "Tantulset",
+                Username = "littlepunchhz",
+                CreateAt = DateTime.UtcNow,
+                UserType = 1,
+                IsLoginWithGoogle = false
+            };
+
+            var mockRequest = new UpdateUserProfileModel()
+            {
+                UserId = mockUser.Id,
+                Username = "littlepunchhzzz",
+                FirstName = "Supansa",
+                LastName = "Tantulset"
+            };
+
+            // setup database response
+            mockEntityUnitOfWork.Setup(x => x.UserRepository.GetSingleAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                                .ReturnsAsync(() => mockUser);
+            mockEntityUnitOfWork.Setup(x => x.UserRepository.Update(It.IsAny<User>()));
+            mockEntityUnitOfWork.Setup(x => x.SaveAsync());
+
+            // act
+            var actualOutput = await authenticationService.UpdateUserProfile(mockRequest);
+
+            // assert
+            mockEntityUnitOfWork.VerifyAll();
+            Assert.True(actualOutput);
+        }
+
+        [Fact]
+        public async Task UpdateUserProfile_ThrowArgumentException_WhenUserDoesNotExist()
+        {
+            // setup database response
+            mockEntityUnitOfWork.Setup(x => x.UserRepository.GetSingleAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                                .ReturnsAsync(() => null);
+
+            // arrange
+            var mockRequest = new UpdateUserProfileModel()
+            {
+                UserId = Guid.NewGuid(),
+                Username = "littlepunchhz",
+                FirstName = "Supansa",
+                LastName = "Tantulset"
+            };
+
+            // act
+            Func<Task> act = () => authenticationService.UpdateUserProfile(mockRequest);
+
+            // assert
+            var exception = await Assert.ThrowsAsync<ArgumentException>(act);
+            mockEntityUnitOfWork.VerifyAll();
+            Assert.Equal("User does not exists.", exception.Message);
+        }
+    }
+}
