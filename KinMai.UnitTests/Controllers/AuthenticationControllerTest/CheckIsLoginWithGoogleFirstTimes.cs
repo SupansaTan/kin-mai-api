349da14454e7c2b36f5366ec744b8bf5a9c97399
Supansa Tantulset <supansa.tantulset@gmail.com>
2023-03-23 02:14:58 +0700

    add test check is login with google first time

---
+++ KinMai.UnitTests/Controllers/AuthenticationControllerTest/CheckIsLoginWithGoogleFirstTimes.cs
@@ -0,0 +1,103 @@
+﻿using KinMai.Api.Controllers;
+using KinMai.Api.Models;
+using KinMai.Authentication.UnitOfWork;
+using KinMai.Dapper.Interface;
+using KinMai.EntityFramework.Models;
+using KinMai.EntityFramework.UnitOfWork.Interface;
+using KinMai.Logic.UnitOfWork.Implement;
+using KinMai.Logic.UnitOfWork.Interface;
+using KinMai.Mail.UnitOfWork;
+using KinMai.S3.UnitOfWork.Interface;
+using KinMai.UnitTests.Shared;
+using Moq;
+using System.Linq.Expressions;
+
+namespace KinMai.UnitTests.Controllers.AuthenticationControllerTest
+{
+    public class CheckIsLoginWithGoogleFirstTimes
+    {
+        private readonly InitConfiguration initConfiguration;
+        private readonly Mock<IDapperUnitOfWork> mockDapperUnitOfWork;
+        private readonly Mock<IS3UnitOfWork> mockS3UnitOfWork;
+        private readonly Mock<IMailUnitOfWork> mockMailUnitOfWork;
+        private readonly Mock<IAuthenticationUnitOfWork> mockAuthenticationUnitOfWork;
+        private readonly Mock<IEntityUnitOfWork> mockEntityUnitOfWork;
+        private readonly ILogicUnitOfWork logicUnitOfWork;
+        private readonly AuthenticationController authenticationController;
+
+        public CheckIsLoginWithGoogleFirstTimes()
+        {
+            initConfiguration = new InitConfiguration();
+            mockDapperUnitOfWork = new Mock<IDapperUnitOfWork>();
+            mockS3UnitOfWork = new Mock<IS3UnitOfWork>();
+            mockMailUnitOfWork = new Mock<IMailUnitOfWork>();
+            mockAuthenticationUnitOfWork = new Mock<IAuthenticationUnitOfWork>();
+            mockEntityUnitOfWork = new Mock<IEntityUnitOfWork>();
+            logicUnitOfWork = new LogicUnitOfWork(
+                mockEntityUnitOfWork.Object,
+                mockDapperUnitOfWork.Object,
+                mockAuthenticationUnitOfWork.Object,
+                mockS3UnitOfWork.Object,
+                mockMailUnitOfWork.Object
+            );
+            authenticationController = new AuthenticationController(logicUnitOfWork);
+        }
+
+        [Fact]
+        public async Task CheckIsLoginWithGoogleFirstTimes_ReturnStatus200_WhenUserIsExist()
+        {
+            // arrange
+            var mockUser = new User()
+            {
+                Id = Guid.NewGuid(),
+                Email = "nampunch1@gmail.com",
+                FirstName = "Supansa",
+                LastName = "Tantulset",
+                Username = "littlepunchhz",
+                CreateAt = DateTime.UtcNow,
+                UserType = 1,
+                IsLoginWithGoogle = true
+            };
+
+            // setup database response
+            mockEntityUnitOfWork.Setup(x => x.UserRepository.GetSingleAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                                .ReturnsAsync(() => mockUser);
+
+            // act
+            var actualOutput = await authenticationController.CheckIsLoginWithGoogleFirstTimes(mockUser.Email);
+            var expectOutput = new ResponseModel<bool>()
+            {
+                Data = false,
+                Message = "success",
+                Status = 200
+            };
+
+            // assert
+            Assert.Equal(expectOutput.Data, actualOutput.Data);
+            Assert.Equal(expectOutput.Message, actualOutput.Message);
+            Assert.Equal(expectOutput.Status, actualOutput.Status);
+        }
+
+        [Fact]
+        public async Task CheckIsLoginWithGoogleFirstTimes_ReturnStatus200_WhenUserDoesNotExist()
+        {
+            // setup database response
+            mockEntityUnitOfWork.Setup(x => x.UserRepository.GetSingleAsync(It.IsAny<Expression<Func<User, bool>>>()))
+                                .ReturnsAsync(() => null);
+
+            // act
+            var actualOutput = await authenticationController.CheckIsLoginWithGoogleFirstTimes("nampunch1@gmail.com");
+            var expectOutput = new ResponseModel<bool>()
+            {
+                Data = true,
+                Message = "success",
+                Status = 200
+            };
+
+            // assert
+            Assert.Equal(expectOutput.Data, actualOutput.Data);
+            Assert.Equal(expectOutput.Message, actualOutput.Message);
+            Assert.Equal(expectOutput.Status, actualOutput.Status);
+        }
+    }
+}
